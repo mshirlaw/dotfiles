b84e9a68ad071d6793b70b491a60c44ce6c009ba
Matt Shirlaw <matt.shirlaw@gmail.com>
2020-02-10 15:54:55 +1100

    Added symlinks for vscode

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/.ackrc ~/.ackrc
+ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/snippets ~/.config/Code/User/snippets
 
 # setup oh my zsh & zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
