a34b05be01dcc10ddf980b0ae59db884a5d35668
Matt Shirlaw <matt.shirlaw@gmail.com>
2023-04-28 09:03:47 +1000

    Add lazyvim config

---
+++ lazyvim/.config/nvim/lua/plugins/nightowl.lua
@@ -0,0 +1,3 @@
+return {
+  "haishanh/night-owl.vim",
+}
---
+++ lazyvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    highlight = {
+      enabled = true,
+    },
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luap",
+      "markdown",
+      "python",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+  },
+}
