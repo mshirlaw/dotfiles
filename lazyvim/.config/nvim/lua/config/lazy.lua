6bcd70bce151121c98a2c9bb1a0c560aa81d4250
Matt Shirlaw <matt.shirlaw@gmail.com>
2023-05-02 11:06:13 +1000

    Update lazyvim config

--- lazyvim/.config/nvim/lua/config/lazy.lua
+++ lazyvim/.config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
   },
@@ -43,9 +44,5 @@ require("lazy").setup({
         "zipPlugin",
       },
     },
-  },
-  {
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
   },
 })
